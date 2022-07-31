2432f51b701ff85a47c9162d8b94877ec6492df3
Stien Vanhoutte <STIEN@VANHOUTTE.EU.COM>
2022-07-31 17:45:56 +0000

    add score and feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const image = document.getElementById("image");
 const instructions=document.getElementById("instructions")
 
 let shuffledQuestions, currentQuestionIndex
+let scoreBox= document.getElementById("score")
 
 // eventlistener, start and next button 
 startButton.addEventListener('click', startGame)
@@ -16,17 +17,24 @@ nextButton.addEventListener('click', () => {
   setNextQuestion()
 })
 
-// startgame, shuffle questions, hide start button and show questionfield
+// increment score, code from love math project from Code institute
+function incrementScore() {
+  let oldScore= parseInt(document.getElementById("score-number").innerText);
+  document.getElementById("score-number").innerText = ++oldScore;
+}
+
+// startgame, randomize questions, hide startbutton + instructions and show questionfield
 function startGame() {
   startButton.classList.add('hide');
   instructions.classList.add('hide');
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  scoreBox.classList.add("hide")
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
 
-//Go to next question
+//reset state and show next
 function setNextQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
@@ -42,13 +50,14 @@ function showQuestion(question) {
     button.innerText = answer.text
     button.classList.add('btn')
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
   })
 }
 
+/*reset to default state*/ 
 function resetState() {
   clearStatusClass(document.body)
   nextButton.classList.add('hide')
@@ -57,25 +66,29 @@ function resetState() {
   }
 }
 
+
+// check selected answer if correct increment score
 function selectAnswer(e) {
-  const selectedButton = e.target;
-  const correct = selectedButton.dataset.correct;
+  let selectedButton = e.target
+  let correct = selectedButton.dataset.correct;
+  if(correct){incrementScore()}
   Array.from(answerButtonsElement.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   } else {
+    feedback();
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
-    questionContainerElement.classList.add('hide');
-  }
+    questionContainerElement.classList.add('hide')}
 }
 
+/* add correct and incorrect class to answers*/ 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
+  clearStatusClass(element);
+  if (correct){
+    element.classList.add('correct');
   } else {
     element.classList.add('wrong')
   }
@@ -84,6 +97,23 @@ function setStatusClass(element, correct) {
 function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
+}
+
+function feedback(){
+let endScore=parseInt(document.getElementById("score-number").innerText)
+let feedback= document.getElementById("feedback")
+scoreBox.classList.remove("hide")
+if(endScore<1){feedback.innerText="Hmm... you may need to practice a bit more."}
+else if  (endScore<2){feedback.innerText="Keep on Learning."} 
+else if  (endScore<3){feedback.innerText="With some excercise you can become a expert!"} 
+else if  (endScore<4){feedback.innerText="This is a good beginning...keep on practicing."} 
+else if  (endScore<5){feedback.innerText="You are on the right pad... with some excercise you will become a true biologist."} 
+else if  (endScore<6){feedback.innerText="Great work!"} 
+else if  (endScore<7){feedback.innerText="Very good!"} 
+else if  (endScore<8){feedback.innerText="You did a great job!"} 
+else if  (endScore<9){feedback.innerText="Wow... amazing! Almost Perfect!"} 
+else if  (endScore<10){feedback.innerText="Wow...perfect!...You are a true biologist!"} 
+else{}
 }
 
 //quiz Questions
